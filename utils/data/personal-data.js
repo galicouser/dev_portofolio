1fcbe94359318efd76e06e07bd3bbf98b14a52ef
Fahad Bashir <fahadbashir757@gmail.com>
2024-10-17 23:53:24 +0500

    init push

---
+++ utils/data/personal-data.js
@@ -0,0 +1,17 @@
+export const personalData = {
+  name: "Galico",
+  profile: '',
+  designation: "Software Company",
+  description: "At Galico, we specialize in innovative web solutions with a passion for JavaScript. As skilled and enthusiastic developers, we constantly explore new technologies to build user-friendly and efficient applications. We're always open to opportunities that match our expertise. Let’s build something great together!",
+  email: 'admin@galico.io',
+  phone: '+17324878977',
+  address: '1515 Market St #1650, Philadelphia, PA 19102',
+  github: 'https://github.com/galicouser',
+  facebook: 'https://www.facebook.com/',
+  linkedIn: 'https://www.linkedin.com/in/galico-software-82b815333/',
+  twitter: 'https://twitter.com/',
+  stackOverflow: 'https://stackoverflow.com/',
+  leetcode: "https://leetcode.com/",
+  devUsername: "galiconetworks",
+  resume: ""
+}
---
+++ utils/data/projects-data.js
@@ -0,0 +1,58 @@
+// import maggiesMagicTouch from '/public/image/maggies-magic-touch.jpg';
+// import casselloJewellers from '/public/image/cassello-jewellers.jpg';
+// import lessinLaw from '/public/image/lessin-law.jpg';
+// import suiWallet from '/public/image/sui-wallet.jpg';
+// import cutTheCable from '/public/image/cut-the-cable.jpg';
+
+export const projectsData = [
+    {
+        id: 1,
+        name: 'Maggies Magic Touch',
+        description: "A cleaning services platform with user-friendly booking and management features.",
+        tools: ['React', 'Node.js','Vite', 'MongoDB'],
+        role: 'Full Stack Developer',
+        code: '',
+        demo: 'https://maggiesmagictouch.com',
+        // image: maggiesMagicTouch,
+    },
+    {
+        id: 2,
+        name: 'Cassello Jewellers',
+        description: 'An AR-enabled e-commerce store for jewelry, offering an immersive shopping experience.',
+        tools: ['React', 'Three.js', 'Node.js'],
+        role: 'Front End Developer',
+        code: '',
+        demo: 'https://cassellojewellers.com',
+        // image: casselloJewellers,
+    },
+    {
+        id: 3,
+        name: 'LessinLaw',
+        description: 'A lawyer website with live chat, appointment booking, and online consultation services.',
+        tools: ['React', 'PHP', 'LiveChat'],
+        role: 'Web Developer',
+        code: '',
+        demo: 'https://lessinlaw.com',
+        // image: lessinLaw,
+    },
+    {
+        id: 4,
+        name: 'Sui Wallet',
+        description: 'A React-based wallet frontend built using Vite, offering a seamless user experience.',
+        tools: ['React', 'Vite', 'JavaScript'],
+        role: 'Frontend Developer',
+        code: '',
+        demo: 'https://sui.galico.io',
+        // image: suiWallet,
+    },
+    {
+        id: 5,
+        name: 'Cut The Cable',
+        description: 'TV cable service with an admin portal for managing user subscriptions and a frontend for user details.',
+        tools: ['React', 'Node.js', 'Stripe'],
+        role: 'Full Stack Developer',
+        code: '',
+        demo: 'https://cutthecable.com',
+        // image: cutTheCable,
+    },
+];
