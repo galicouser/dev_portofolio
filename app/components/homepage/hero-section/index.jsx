cbcca20b3689326c74b9eb2c8437a5e64d79b029
Fahad Bashir <fahadbashir757@gmail.com>
2024-10-18 05:37:09 +0500

    changes to project page

--- app/components/homepage/hero-section/index.jsx
+++ app/components/homepage/hero-section/index.jsx
@@ -64,9 +64,9 @@ function HeroSection() {
               href={personalData.twitter}
               target='_blank'
               className="transition-all text-pink-500 hover:scale-125 duration-300"
-            > */}
+            >
               <FaTwitterSquare size={30} />
-            </Link>
+            </Link> */}
           </div>
 
           <div className="flex items-center gap-3">
